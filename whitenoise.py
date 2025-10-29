ec1c5081ee486739464302620fa71839abdae8e3
jp1593 <juanpablo08082002@hotmail.com>
2025-10-28 18:18:58 -0600

    feat: whitenoise exercise

---
+++ whitenoise.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.graphics.tsaplots import plot_acf
+
+# Simulation parameters
+T = 100           # Length of the time series
+mu = 0            # Mean of the noise
+sigma = 1         # Standard deviation of the noise
+np.random.seed(42) 
+
+
+# White Noise Serie
+X = np.random.normal(mu, sigma, T)
+
+# Plot - Time Serie
+plt.figure(figsize=(10, 4))
+plt.plot(X, marker='o', linestyle='-', color='blue')
+plt.title("White Noise Time Series")
+plt.xlabel("Time t")
+plt.ylabel("X_t")
+plt.grid(True)
+plt.show()
+
+# Plot - Autocorrelation sample 
+# plt.figure(figsize=(10, 4))
+plot_acf(X, lags=20, alpha=0.05) 
+plt.title("Sample Autocorrelation Function r(h)")
+plt.xlabel("Lag h")
+plt.ylabel("r(h)")
+plt.show()
